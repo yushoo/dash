7d38b9792f13d7a4126af0a08480830b65d6e9e4
Henry Feng <henryfeng12345@gmail.com>
2019-11-26 16:59:31 -0600

    add hooks for join.js and add style

--- server/index.js
+++ server/index.js
@@ -10,6 +10,15 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+    console.log('We have a new connection');
+
+    socket.on('disconnect', () => {
+        console.log('User has left!');
+    });
+});
+
+//Route set up for '/'
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server has start on ${PORT}`));
