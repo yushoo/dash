c75ee2251f6282775b3ea0fe892588b476241b8d
Henry Feng <henryfeng12345@gmail.com>
2019-11-28 13:08:26 -0600

    add users.js in server

---
+++ server/users.js
@@ -0,0 +1,19 @@
+//create helper functions to help us manage anything related to users
+
+const users = [];
+
+const addUser = () => {
+
+}
+
+const removeUser = () => {
+
+}
+
+const getUser = () => {
+
+}
+
+const getUsersInRoom = () => {
+    
+}
