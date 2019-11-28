625c5a9811b6f3fd48cff62b80e9193d19fa8c0e
Henry Feng <henryfeng12345@gmail.com>
2019-11-28 13:14:10 -0600

    create addUser function in users.js

--- server/users.js
+++ server/users.js
@@ -2,8 +2,26 @@
 
 const users = [];
 
-const addUser = () => {
+const addUser = ({id, name, room}) => {
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
 
+    const existingUser = users.find((user) => user.room === room
+                                            && user.name === name);
+    
+    //check if new user                                        
+    if(existingUser){
+        return {error: 'Username is taken'};
+    }
+
+    //create new user
+    const user = {id, name, room};
+
+    //push new user onto array of users
+    users.push(user);
+
+    //return new user so we know who the new user is
+    return { user };
 }
 
 const removeUser = () => {
@@ -15,5 +33,5 @@ const getUser = () => {
 }
 
 const getUsersInRoom = () => {
-    
+
 }
