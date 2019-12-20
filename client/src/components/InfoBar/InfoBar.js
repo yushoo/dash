1dc2220958d2b634c166cd85c30d97308e3f8128
Henry Feng <henryfeng12345@gmail.com>
2019-12-20 17:07:34 -0600

    add icon to infoBar

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+//icon images
+import closeIcon from '../../icons/closeIcon.png';
+import closeIcon from '../../icons/onlineIcon.png';
+
+
 import './InfoBar.css';
 
 const InfoBar = () => {
-    
+    <div className="infoBar">
+        <div className="leftInnerContainer">
+            <img className="onlineIcon" src={onlineIcon} alt="online image" />
+            <h3>roomName</h3>
+        </div>
+        <div className="RightInnerContainer">
+            <a href="/"><img src={closeIcon} alt="close image" /></a>
+        </div>
+    </div>
 }
 
 export default InfoBar;
