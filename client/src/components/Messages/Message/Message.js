7b52d53f804163a92030fbfe2630c8e61023797e
Henry Feng <henryfeng12345@gmail.com>
2019-12-21 17:25:43 -0600

    add react emoji

--- client/src/components/Messages/Message/Message.js
+++ client/src/components/Messages/Message/Message.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './Message.css';
 
+import ReactEmoji from 'react-emoji';
+
 const Message = ({ message: {user, text}, name}) => {
     let isSentByCurrentUser = false;
 
     const trimmedName = name.trim().toLowerCase();
-    
+
     if(user === trimmedName){
         isSentByCurrentUser = true;
     }
@@ -17,14 +19,14 @@ const Message = ({ message: {user, text}, name}) => {
             <div className="messageContainer justifyEnd">
                 <p className="sentText pr-10">{trimmedName}</p>
                 <div className="messageBox backgroundBlue">
-                  <p className="messageText">{text}</p>
+                  <p className="messageText">{ReactEmoji.emojify(text)}</p>
                 </div>
             </div>
         ) 
         : (
             <div className="messageContainer justifyStart">
                 <div className="messageBox backgroundLight">
-                  <p className="messageText colorDark">{text}</p>
+                  <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
                 </div>
                 <p className="sentText pl-10">{user}</p>
             </div>
