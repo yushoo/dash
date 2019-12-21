0669ca6607983fc15abfe8a519c1b170fe6553cb
Henry Feng <henryfeng12345@gmail.com>
2019-12-21 16:04:53 -0600

    add Input component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,6 +9,7 @@ import io from "socket.io-client";
 
 import './Chat.css';
 import InfoBar from '../InfoBar/InfoBar';
+import Input from '../InfoBar/InfoBar';
 
 let socket;
 
@@ -72,7 +73,7 @@ const Chat = ({ location }) => {
       <div className="container">
           {/* pass room into infoBar */}
           <InfoBar room={room} />
-          // {/* <Messages messages={messages} name={name} />
+           {/* <Messages messages={messages} name={name} />
           // <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />  */}
           <input 
             value = {message}
---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './Input.css';
+
+const Input = ({ room }) => (
+    <form>
+    </form>
+)
+
+export default Input;
