0d2a3c295b976119074e8d8370c619cfbc7e9278
Henry Feng <henryfeng12345@gmail.com>
2019-12-26 19:13:21 -0600

    Updates

---
+++ precache-manifest.cb99b63380785be62b4f831f73cedcaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571d9fb2abce5e166b4cee36386d64ae",
+    "url": "/dash/index.html"
+  },
+  {
+    "revision": "2e7b8866d70606960fbf",
+    "url": "/dash/static/css/main.83597818.chunk.css"
+  },
+  {
+    "revision": "90fbcc9238f6cf25110d",
+    "url": "/dash/static/js/2.d5029d23.chunk.js"
+  },
+  {
+    "revision": "e5fdd3c10a39c54f9723efcb1214ee6a",
+    "url": "/dash/static/js/2.d5029d23.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e7b8866d70606960fbf",
+    "url": "/dash/static/js/main.be2bc0bd.chunk.js"
+  },
+  {
+    "revision": "d46f3f3facddc681abc8",
+    "url": "/dash/static/js/runtime-main.c5f5d1b1.js"
+  }
+]);
